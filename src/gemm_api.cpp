adeeed0a9fa142dc4fed11c872e8ffa88ac81974
Jing Zhou <jingz@amd.com>
2020-07-09 15:08:52 -0700

    rocblas strategy

--- src/gemm_api.cpp
+++ src/gemm_api.cpp
@@ -2,8 +2,12 @@
 #include <Tensile/Tensile.hpp>
 #include <Tensile/Contractions.hpp>
 #include <Tensile/EmbeddedLibrary.hpp>
+#include <Tensile/MasterSolutionLibrary.hpp>
+#include <Tensile/TensorDescriptor.hpp>
+#include <Tensile/Utils.hpp>
 #include <Tensile/hip/HipHardware.hpp>
 #include <Tensile/hip/HipSolutionAdapter.hpp>
+#include <Tensile/hip/HipUtils.hpp>
 #include <dlfcn.h>
 #include <glob.h>
 
@@ -149,14 +153,135 @@ miopen_tensile_status miopen_tensile_gemm_hip(hipStream_t stream,
                                               double alpha, 
                                               double beta)
 {
-    auto problem = create_tensile_problem(deref(b), deref(a), deref(c));
-    auto hardware = Tensile::hip::GetCurrentDevice();
-    auto solution = library().findBestSolution(problem, *hardware);
-    if (not solution)
-    {
-        std::cerr << "No solution found." << std::endl;
-        return miopen_tensile_status_no_solution;
-    }
+
+    miopen_tensile_status status = miopen_tensile_status_unknown;
+    std::shared_ptr<Tensile::ContractionSolution> solution;
+
+        std::shared_ptr<Tensile::MasterSolutionLibrary<Tensile::ContractionProblem>> library;
+        std::shared_ptr<Tensile::Hardware>                                           hardware;
+        Tensile::hip::SolutionAdapter                                                adapter;
+
+hardware = Tensile::hip::GetCurrentDevice();
+
+std::string dir = library_path() + "*co";
+            glob_t glob_result{};
+            int    g = glob(dir.c_str(), GLOB_NOSORT, nullptr, &glob_result);
+            if(!g)
+            {
+                for(size_t i = 0; i < glob_result.gl_pathc; ++i)
+                    adapter.loadCodeObjectFile(glob_result.gl_pathv[i]);
+            }
+
+library = std::dynamic_pointer_cast<
+                Tensile::MasterSolutionLibrary<Tensile::ContractionProblem>>(
+                Tensile::LoadLibraryFile<Tensile::ContractionProblem>(library_path() + "TensileLibrary.yaml"));
+
+
+        static constexpr Tensile::DataType Tensile_Ti = Tensile::DataType::Float;
+        static constexpr Tensile::DataType Tensile_To = Tensile::DataType::Float;
+
+        // Tensor descriptors for ta, tb
+        Tensile::TensorDescriptor ta, tb;
+
+        // Tensor ops for matrices, like complex conjugate
+        Tensile::TensorOps aops, bops, cops, dops;
+
+        // Tensile Indices for contraction problem
+        Tensile::ContractionProblem::FreeIndices  freeIndex(2);
+        Tensile::ContractionProblem::BoundIndices boundIndex(1);
+        Tensile::ContractionProblem::BatchIndices batchIndex{{2, 2, 2, 2}};
+
+        // Set up GEMM indices
+        freeIndex[0].isA = true;
+        freeIndex[1].isA = false;
+        freeIndex[0].c = freeIndex[0].d = 0;
+        freeIndex[1].c = freeIndex[1].d = 1;
+
+        // Tensile does not support 0-sized dimensions. For when k == 0, we still need to
+        // multiply C by beta, but not add any of the rank-0 dot products. As a workaround,
+        // we pass k = 1 and set alpha == 0, since alpha == 0 has the same effect as k == 0.
+        auto k = is_transposed(a) ? a.lens[1] : a.lens[0];
+        k = k == 0 ? 1 : k;
+
+        // If A is transposed, swap the free and bound dimensions and their ranks
+        if(is_transposed(a))
+        {
+            ta = {
+                    Tensile_Ti,
+                    {k, a.lens[0], a.batch.num},
+                    {1, get_ld(a), a.batch.stride}
+                };
+            freeIndex[0].i  = 1;
+            boundIndex[0].a = 0;
+        }
+        else
+        {
+            ta = {
+                    Tensile_Ti,
+                    {a.lens[1], k, a.batch.num},
+                    {1, get_ld(a), a.batch.stride}
+                };
+            freeIndex[0].i  = 0;
+            boundIndex[0].a = 1;
+        }
+
+        // If B is transposed, swap the free and bound dimensions and their ranks
+        if(is_transposed(b))
+        {
+            tb = {
+                    Tensile_Ti,
+                    {b.lens[1], k, b.batch.num},
+                    {1, get_ld(b), b.batch.stride}
+                };
+            freeIndex[1].i  = 0;
+            boundIndex[0].b = 1;
+        }
+        else
+        {
+            tb = {
+                    Tensile_Ti,
+                    {k, b.lens[0], b.batch.num},
+                    {1, get_ld(b), b.batch.stride}
+                };
+            freeIndex[1].i  = 1;
+            boundIndex[0].b = 0;
+        }
+
+
+        // Descriptor for input matrix C
+        Tensile::TensorDescriptor tc{Tensile_To,
+                                    {is_transposed(a) ? a.lens[0] : a.lens[1], is_transposed(b) ? b.lens[1] : b.lens[0], c.batch.num},
+                                    {1, get_ld(c), c.batch.stride}};
+
+        // Descriptor for output matrix D
+        Tensile::TensorDescriptor td{Tensile_To,
+                                    {is_transposed(a) ? a.lens[0] : a.lens[1], is_transposed(b) ? b.lens[1] : b.lens[0], c.batch.num},
+                                    {1, get_ld(c), c.batch.stride}};
+
+        // The ContractionProblem
+        Tensile::ContractionProblem tensileProblem{ta,
+                                                   aops,
+                                                   tb,
+                                                   bops,
+                                                   tc,
+                                                   cops,
+                                                   td,
+                                                   dops,
+                                                   freeIndex,
+                                                   batchIndex,
+                                                   boundIndex,
+                                                   beta};
+
+        // If HPA is active, mark it as true
+        if(sizeof(Tc) > sizeof(Ti))
+            tensileProblem.setHighPrecisionAccumulate(true);
+
+
+
+
+
+        solution          = library->findBestSolution(tensileProblem, *hardware);
+
     Tensile::TypedContractionInputs<float> inputs;
     inputs.a = reinterpret_cast<const float*>(b->data);
     inputs.b = reinterpret_cast<const float*>(a->data);
@@ -164,9 +289,22 @@ miopen_tensile_status miopen_tensile_gemm_hip(hipStream_t stream,
     inputs.d = reinterpret_cast<float*>(c->data);
     inputs.alpha = alpha;
     inputs.beta = beta;
-    auto kernels = solution->solve(problem, inputs, *hardware);
-    adaptor().launchKernels(kernels, stream, nullptr, nullptr);
-    return miopen_tensile_status_success;
+
+        if(!solution)
+        {
+            td::cerr << "No solution found." << std::endl;
+            status          = miopen_tensile_status_no_solution;
+        }
+        else
+        {
+            auto handle = prob.handle;
+            adapter.launchKernels(
+                solution->solve(tensileProblem, inputs, *hardware),
+                stream, nullptr, nullptr);
+            status = rocblas_status_success;
+        }
+
+    return status;
 }
 
 }
